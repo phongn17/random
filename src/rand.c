88525009e1da5a84bb394dfaea634cd427e99219
phongn17 <phongnguyen17@yahoo.com>
2018-01-12 23:55:20 -0800

    Abstract RNG initilization

--- src/rand.c
+++ src/rand.c
@@ -18,8 +18,8 @@ void init_rand(void) {
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-    fprintf(stderr, "Usage: %s <number>\n", argv[0]);
-    return EXIT_FAILURE;
+        fprintf(stderr, "Usage: %s <number>\n", argv[0]);
+        return EXIT_FAILURE;
     }
     int max = atoi(argv[1]);
     int result = random_int(max);
